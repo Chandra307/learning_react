759d89988826734428846e29af5553f65c068254
Chandra307 <chandra19151425@gmail.com>
2024-03-26 16:21:50 +0530

    first custom component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h1>Getting started with React.</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
