9f2d8865140dda138411e2ea90f27c3d605aedb5
Chandra307 <chandra19151425@gmail.com>
2024-03-26 17:42:04 +0530

    writing commplex jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,22 @@
 export default function ExpenseItem() {
     return (
+        // As an object
         <div>
-            <p>Food - $5</p>
-            <p>Petrol - $20</p>
-            <p>Movie - $10</p>
+            <p>15 August 2023</p>
+            <p>Delhi</p>
+            <div>
+                <h2>Book</h2>
+                <p>$10</p>
+            </div>
         </div>
+        // Or as an array
+        // <>
+        //     <p>15 August 2023</p>
+        //     <p>Delhi</p>
+        //     <div>
+        //         <h2>Book</h2>
+        //         <p>$10</p>
+        //     </div>
+        // </>
     );
 }
